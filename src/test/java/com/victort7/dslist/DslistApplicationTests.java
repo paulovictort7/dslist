174e41ee94ea706916d65319835f142b218fa76b
Paulo Victor <paulovictor2536@gmail.com>
2024-11-15 22:56:53 -0300

    Project created

---
+++ src/test/java/com/victort7/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victort7.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
